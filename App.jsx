c51d6496638bac9c3a67c4c667674e2c4c939a26
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-04-30 10:27:57 -0400

    Add files via upload
    A simple Digital Clock using ReactJS

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import DigitalClock from "./components/Digital_Clock/DigitalClock";
+function App() {
+    return (
+    <>
+    <DigitalClock />
+    </>
+    );
+}
+
+export default App;
